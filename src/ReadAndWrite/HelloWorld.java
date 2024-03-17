be2547fee6df7dc9a243fa2c19710c0416a793bb
구동엽 <ehd_duq94@naver.com>
2024-03-17 16:29:21 +0900

    feat : 입출력 연습

--- src/ReadAndWrite/HelloWorld.java
+++ src/ReadAndWrite/HelloWorld.java
@@ -1,15 +1,15 @@
+package ReadAndWrite;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class test {
+public class HelloWorld {
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         bufferedWriter.write("Hello World!");
 
         bufferedWriter.flush();
         bufferedWriter.close();
-
-
     }
 }
