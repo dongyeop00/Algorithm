423fb19da1999d7f9293d44bd060887a2254d625
dongyeop00 <ehd_duq94@naver.com>
2024-03-17 12:50:46 +0900

    feat : do it 알고리즘

--- src/test.java
+++ src/test.java
@@ -1,12 +1,14 @@
-import java.io.*;
-import java.util.StringTokenizer;
 
 public class test {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
+    public static void main(String[] args){
+        int n = 100000;
+        int cnt = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;j++) {
+                System.out.println("연산 횟수 :" + cnt++);
+            }
+        }
 
     }
 }
