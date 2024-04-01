6adffec3bc3242737cb35586b41250256a649572
구동엽 <ehd_duq94@naver.com>
2024-04-01 17:34:33 +0900

    feat : Algo 스터디

---
+++ src/Algo/Baek11899.java
@@ -0,0 +1,13 @@
+package Algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek11899 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = bufferedReader.readLine();
+    }
+}
