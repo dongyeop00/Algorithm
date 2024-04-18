ace92753bbba82847316150427fd79e33f459da1
구동엽 <ehd_duq94@naver.com>
2024-04-18 21:11:06 +0900

    feat : 다시 풀기

--- src/DFS/Baek2468.java
+++ src/DFS/Baek2468.java
@@ -10,13 +10,17 @@ public class Baek2468 {
 
     static boolean[][] visited;
     static int[][] V;
+    static int count = 0;
+    static int N;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        int N = Integer.parseInt(stringTokenizer.nextToken());
+        N = Integer.parseInt(stringTokenizer.nextToken());
 
         visited = new boolean[N][N];
         V = new int[N][N];
@@ -24,10 +28,40 @@ public class Baek2468 {
         for(int i=0; i<N; i++){
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             for(int j=0; j<N; j++){
-                V[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                int temp = Integer.parseInt(stringTokenizer.nextToken());
+                if(temp <= N){
+                    V[i][j] = 0;
+                }else{
+                    V[i][j] = temp;
+                }
             }
         }
 
+        for(int i=0; i<N; i++){
+            for (int j = 0; j < N; j++) {
+                if(V[i][j] > 5 && !visited[i][j]) {
+                    DFS(i, j);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void DFS(int x, int y){
+        visited[x][y] = true;
+
+        for(int i=0; i<4; i++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            //cx >= 0 && cy >= 0 && cx < M && cy < N
+            if(newX >= 0 && newY >= 0 && newX < N && newY < N){
+                if(V[newX][newY] > 5 && !visited[newX][newY]) {
+                    DFS(newX, newY);
+                }
+            }
+        }
     }
 
 
