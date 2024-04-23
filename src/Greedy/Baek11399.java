14d13669c57e89475fc7d5191c0a68a06b02fb81
구동엽 <ehd_duq94@naver.com>
2024-04-23 23:32:15 +0900

    feat : Greedy

--- src/Greedy/Baek11399.java
+++ src/Greedy/Baek11399.java
@@ -1,4 +1,35 @@
 package Greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Baek11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int[] time = new int[N];
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(time);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += time[j];
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
