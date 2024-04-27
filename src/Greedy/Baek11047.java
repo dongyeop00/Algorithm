1b5fa5d2d7519c2237ca27ef1ad7792fab59add0
dongyeop00 <ehd_duq94@naver.com>
2024-04-27 16:21:38 +0900

    feat : 동전 0

--- src/Greedy/Baek11047.java
+++ src/Greedy/Baek11047.java
@@ -1,4 +1,33 @@
 package Greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Baek11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+
+        int[] coin = new int[N];
+        int count = 0;
+
+        for(int i=0; i<N; i++){
+            coin[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        for(int i = N-1; i>=0; i--){
+            if(coin[i] <= K){
+                count += K/coin[i];
+                K %= coin[i];
+            }
+        }
+
+        System.out.println(count);
+    }
 }
